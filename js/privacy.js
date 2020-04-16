e4fce21669c5449c10d816782fd9ac1501844498
oophzh <oophzh@163.com>
2020-04-16 17:16:05 +0800

    M需求合入

--- js/privacy.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-require.config(requireConfig),require(["jquery","tmpl","i18n","bootstrap","common"],function(i,o,n,r,e){console.log("privacy.js"),e.initLoginStatus()});
--- js/size_guide.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-require.config(requireConfig),require(["jquery","tmpl","i18n","bootstrap","common"],function(i,o,e,n,r){console.log("size_guide.js"),r.initLoginStatus()});
